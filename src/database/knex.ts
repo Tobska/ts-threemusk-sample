66c8586129b8c69a6b30f8aa20042818b96b62fa
Tobska <patrick_tobias@outlook.com>
2024-03-26 15:10:08 +0800

    add env sample

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -3,15 +3,14 @@ import { Knex, knex as knexInit } from "knex";
 const config: Knex.Config = {
   client: "pg",
   connection: {
-    // host: "172.17.0.1",
-    host: "10.0.2.15",
-    port: 5432,
-    database: "sample_db",
-    user: "postgres",
-    password: "postgres",
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     ssl: false,
-    charset: "utf8",
-  },
+    charset: "utf8"
+  }
 };
 
 export const knex = knexInit(config);
